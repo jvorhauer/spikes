31bd36aa2559b67b91265657ff02ed8660d60c40
Jurjen Vorhauer <jurjen@vorhauer.nl>
2024-01-23 17:20:12 +0100

    Prepare for ScalaSQL, jwt-scala and for frontend use.

--- src/main/scala/spikes/model/Status.scala
+++ src/main/scala/spikes/model/Status.scala
@@ -2,5 +2,5 @@ package spikes.model
 
 object Status extends Enumeration {
   type Status = Value
-  val Blank, New, ToDo, Doing, Completed = Value
+  val Blank, New, Ready, ToDo, Doing, Review, Completed = Value
 }
